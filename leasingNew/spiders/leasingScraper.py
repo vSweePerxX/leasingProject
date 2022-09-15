8e3da725508ddbf6c1b62152dd80864a876b2c97
Jan Bauer <janbennobauer@gmail.com>
2022-09-15 14:45:59 +0200

    added result folder

--- leasingNew/spiders/leasingScraper.py
+++ leasingNew/spiders/leasingScraper.py
@@ -8,11 +8,11 @@ from leasingNew.items import leasingScraperItem
 class LeasingScraper(scrapy.Spider):
     name = 'leasingCrawler'
 
-    def __init__(self):
-        global test
-        #test = bundesland
-        #self.start_urls = [f'https://www.leasingmarkt.de/listing?v=2&nc=1&mn={bundesland}&sort=popularity']
-        self.start_urls = ['https://www.leasingmarkt.de/listing?v=2&nc=1&mn=9&mlpt=300&sort=popularity']
+    def __init__(self, model=''):
+        global gModel
+        gModel = model
+        self.start_urls = [f'https://www.leasingmarkt.de/listing?v=2&nc=1&mn={model}&sort=popularity']
+        #self.start_urls = ['https://www.leasingmarkt.de/listing?v=2&nc=1&mn=9&mlpt=300&sort=popularity']
 
     def parse(self, response):
         item = leasingScraperItem()
@@ -28,7 +28,7 @@ class LeasingScraper(scrapy.Spider):
                 yield item
 
         for x in range(8):
-            next_page_part = f'https://www.leasingmarkt.de/listing?v=2&nc=1&mn=9&mlpt=300&sort=popularity&p={x}'
+            next_page_part = f'https://www.leasingmarkt.de/listing?v=2&nc=1&mn={gModel}&mlpt=300&sort=popularity&p={x}'
 
             if next_page_part is not None:
                 yield scrapy.Request(next_page_part, callback=self.parse)
@@ -39,6 +39,26 @@ class LeasingScraper(scrapy.Spider):
         #item['Title'] = response.xpath('//div[@class="flex-inline text-3 text-turquoise-120 font-semibold whitespace-nowrap desktop:w-3/4 overflow-hidden truncate overflow-ellipsis"]/text()').get()
 
         #yield item
+
+class MobileScraper(scrapy.Spider):
+    name = 'mobileCrawler'
+    start_urls = ['https://suchen.mobile.de/fahrzeuge/search.html?dam=0&isSearchRequest=true&ref=quickSearch&sb=rel&vc=Car']
+
+
+    def parse(self, response):
+        item = leasingScraperItem()
+        # for results in response.xpath('//li[@layout]'):
+        liste = response.css(".cBox--resultList > .cBox-body")
+
+        for auto in liste:
+            item['Title'] = auto.css(".u-text-break-word::text").get()
+            yield item
+
+       # for x in range(8):
+        #    next_page_part = f'https://www.leasingmarkt.de/listing?v=2&nc=1&mn={gModel}&mlpt=300&sort=popularity&p={x}'
+
+         #   if next_page_part is not None:
+          #      yield scrapy.Request(next_page_part, callback=self.parse)
 
 
 
